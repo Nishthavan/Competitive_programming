4655e022d5566d7109723755796b244a1b44c2b4
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2020-04-17 12:21:09 +0530

    Ok

--- Coding Blocks/Number_Theory/divisible_subarrays.cpp
+++ Coding Blocks/Number_Theory/divisible_subarrays.cpp
@@ -10,20 +10,24 @@ while(tc--){
   cin>>n;
   std::vector<int> v(n);
   std::vector<int> mod(n);
+  int k = n;
   int cumulative_sum = 0;
   int result = 0;
   for (int i = 0; i < n; i++) {
       cin>>v[i];
-      cumulative_sum+=v[i];
-      //DOUBLE MOD BECAUSE NUMBER CAN BE NEGATIVE
-      mod[((cumulative_sum%k)+k)%k]++;
+  }
+  for (int i = 0; i < n; i++) {
+    cumulative_sum+=v[i];
+    mod[((cumulative_sum%k)+k)%k]++;
   }
   for (int i = 0; i < n; i++) {
      if(mod[i]>1){
-       result+= (mod[i]*(mod[i]-1))/2;}
-     //where there is no remainder it is automattically divisible
-     result+= mod[0];
+       result+= (mod[i]*(mod[i]-1))/2;
+
+       }
     }
+    //where there is no remainder it is automattically divisible
+    result+= mod[0];
   cout<<result<<endl;
 }
   return 0;
